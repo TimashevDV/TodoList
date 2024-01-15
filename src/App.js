11ac8ca15522991d7ded886fa42f57ef0787f66d
Dmitry <Timashev_Dima@mail.ru>
2024-01-15 21:44:21 +0400

    add todoList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useState } from 'react';
 import styles from './app.module.css';
+import { useEffect } from 'react';
 
 export const App = () => {
+	const [todoList, setTodoList] = useState([]);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((loadedTodos) => loadedTodos.json())
+			.then((loadedTodo) => {
+				setTodoList(loadedTodo);
+				console.log(loadedTodo);
+			})
+			.catch((error) => console.log(error));
+	}, []);
+
 	return (
 		<div className={styles.app}>
+			<h1>Вывод списка</h1>
+			{todoList.map(({ id, title }) => (
+				<li key={id} className={styles.todoList}>
+					{title}
+				</li>
+			))}
 			<div></div>
 		</div>
 	);
